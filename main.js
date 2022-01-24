96c10ac940345607292555d87b5da8acf881d7ae
camelCaseEnjoyer <sara.hoberg21@gmail.com>
2022-01-24 01:12:07 -0600

    Refactor PinLockManager class/object
    Turn PinLockManager into an object instead of a Class. We won't instantiate multiple PinLockManagers, and the information has to be available globally anyway. We also implement better promise handling by having the send() and unpin() calls execute in parallel, instead of in serial.

--- main.js
+++ main.js
@@ -9,25 +9,22 @@ const DEFAULT_COMMAND_PREFIX = '-';
 const ALPHANUMERICS_WHITESPACE = 'abcdefghijklmnopqrstuvwxyz1234567890 \t\n';
 const DEFAULT_MAX_PINS = 40;
 
-class PinLockManager {
-	_lockedChannels;
-	constructor () {
-		this._lockedChannels = [];
-	}
-	
+let PinLockManager = {
+	_lockedChannels : [],
 	async updateChannelPins(channel) {
 		if (this._lockedChannels.indexOf(channel.id) != -1) {
 			console.log('Channel is locked. Ending.')
 			return true;
 		}
 		this._lockedChannels.push(channel.id);
-		var maxPins = DEFAULT_MAX_PINS;
+		let maxPins = DEFAULT_MAX_PINS;
+		
 		const serverConfig = await getGuildConfigDoc(channel.guild.id);
 		if(serverConfig && serverConfig.maxPins) {
 			maxPins = serverConfig.maxPins;
 		}
 		let channelConfig = await getChannelConfigDoc(channel.id);
-		var pinboardID;
+		let pinboardID;
 		
 		if(channelConfig && channelConfig.pinboard) {
 			pinboardID = channelConfig.pinboard;
@@ -39,11 +36,16 @@ class PinLockManager {
 			return false;
 		}
 		
-		let pinnedMessages = await channel.messages.fetchPinned();
-		console.log(`pinnedMessages.size: ${pinnedMessages.size}`)
+		let pinboardAndPinsPromise = Promise.all([channel.guild.channels.fetch(pinboardID), channel.messages.fetchPinned()])
 		var unpinMessage;
-		while(pinnedMessages.size > maxPins) {
-			unpinMessage = pinnedMessages.at(-1)
+		let copyAndUnpinPromises = [];
+	
+		// Once the pinboard and pinned messages have been acquired, we put them into vars and get to work.
+		const [pinboard, pins] = await pinboardAndPinsPromise;
+		
+		while(pins.size > maxPins) {
+			// Make a copy of the first pin (chronologically)
+			unpinMessage = pins.at(-1)
 			let copy = copyMessage(unpinMessage);
 			// Notepad++ is mean about this, but we're using three escaped backticks here
 			let sentDate = new Date(unpinMessage.createdTimestamp);
@@ -53,13 +55,17 @@ class PinLockManager {
 			`Time: ${sentDate.toTimeString()}\n` +
 			`\`\`\`\n${copy.content}`
 			
-			await pinboard.send(copy);
-			pinnedMessages.delete(pinnedMessages.lastKey());
-			await unpinMessage.unpin()
+			// Start working on sending the copy and unpinning the original, and keep an array of promises.
+			copyAndUnpinPromises.push(pinboard.send(copy).catch(console.error));
+			copyAndUnpinPromises.push(unpinMessage.unpin().catch(console.error));
+			pins.delete(pins.lastKey());
 		}
-		// Unlock the channel for future pin shenanigans.
-		return true;
+
+		// Unlock the channel for future pin shenanigans only once all the messages have been unpinned and sent.
+		result = await Promise.allSettled(copyAndUnpinPromises);
 		this._lockedChannels.splice(this._lockedChannels.indexOf(channel.id), 1);
+
+		return result;
 	}
 	
 }
@@ -84,7 +90,7 @@ async function updateGuildPins(guild) {
 	for (const pair of channels) {
 		// Text channels only, please.
 		if (pair[1].type == 'GUILD_TEXT') {
-			results.push(pinManager.updateChannelPins(pair[1]));
+			results.push(PinLockManager.updateChannelPins(pair[1]));
 		}
 	}
 	results = await Promise.all(results);
@@ -211,7 +217,7 @@ const COMMAND_MAP = new Map([
 			const collection = dbClient.db(DB_NAME).collection(CHANNEL_CONFIG_NAME);
 			const result = await lazyUpsert(collection, msg.channel.id, {pinboard : newPinboard.id});
 			if (result) {
-				pinManager.updateChannelPins(msg.channel);
+				await PinLockManager.updateChannelPins(msg.channel);
 				return `Change successful! The new pinboard for ${msg.channel.toString()} is ${channelName}`;
 			}
 			else {
@@ -334,9 +340,6 @@ const client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES', 'DIRECT_MESSAG
 // Also create a client to log in to mongodb. Not connected yet.
 const dbClient = new MongoClient(MONGO_URI);
 
-// Create a PinLockManager to make sure channels aren't being edited by multiple function instances at the same time.
-const pinManager = new PinLockManager();
-
 client.once('ready', () => {
     console.log('Connected to Discord.');
 });
@@ -390,8 +393,8 @@ client.on('guildDelete', async function (guild) {
 });
 
 client.on('channelPinsUpdate', async function (channel) {
-	console.log(`Updating pins for ${channel.toString()}`)
-	let result = await pinManager.updateChannelPins(channel);
+	console.log(`Pins changed for ${channel.name}`)
+	let result = await PinLockManager.updateChannelPins(channel);
 	if(result) {
 		console.log('No errors reported');
 	}
